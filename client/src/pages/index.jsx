8471ea46b6739168a7dc7d111c094a153e915f08
HTL <htluu@ucsd.edu>
2024-10-29 19:10:24 -0700

    trips done

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -3,21 +3,11 @@ import styles from "@/styles/Home.module.css";
 const Home = () => {
   return (
     <div className={styles.main}>
-      <div id="navbar">
-        <div class="title">
-          <a href="index.html">HackTravels</a>
-        </div>
-        <div class="navButtons">
-          <a href="addTrip.html">Add Trip</a>
-          <a href="addActivity.html">Add Trip Activity</a>
-        </div>
-      </div>
-
-      <div class="tripsContainer">
-        <div class="tripBox">
+      <div className={styles.tripContainer}>
+        <div className={styles.tripBox}>
           <h3>Trips</h3>
         </div>
-        <div class="tripBox">
+        <div className={styles.tripBox}>
           <h3>Activities</h3>
         </div>
       </div>
