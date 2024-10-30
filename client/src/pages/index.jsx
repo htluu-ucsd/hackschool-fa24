0e6450d5f3bb4e5537b86e81686403e5534e7e5e
HTL <htluu@ucsd.edu>
2024-10-29 18:47:06 -0700

    starting

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -3,6 +3,24 @@ import styles from "@/styles/Home.module.css";
 const Home = () => {
   return (
     <div className={styles.main}>
+      <div id="navbar">
+        <div class="title">
+          <a href="index.html">HackTravels</a>
+        </div>
+        <div class="navButtons">
+          <a href="addTrip.html">Add Trip</a>
+          <a href="addActivity.html">Add Trip Activity</a>
+        </div>
+      </div>
+
+      <div class="tripsContainer">
+        <div class="tripBox">
+          <h3>Trips</h3>
+        </div>
+        <div class="tripBox">
+          <h3>Activities</h3>
+        </div>
+      </div>
     </div>
   );
 };
